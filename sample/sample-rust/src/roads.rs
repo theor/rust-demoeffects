4ea440fcef55e49d4c13ebe578a243a9efa27520
Theo Richart <theor@unity3d.com>
2023-07-14 11:26:34 -0400

    wip roads data

--- sample/sample-rust/src/roads.rs
+++ sample/sample-rust/src/roads.rs
@@ -2,20 +2,27 @@ use line_drawing::Bresenham;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 use bevy::math::*;
-// struct P {
 
-// }
-// struct Seg {
-//     index: usize,
-//     p1: P,
-//     p2: P,
-// }
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+
+struct Seg {
+    index: usize,
+    p1: Vec3,
+    p2: Vec3,
+}
 
 #[wasm_bindgen]
 pub struct Roads {
-    w: usize,
-    h: usize,
-    // segments: Vec<Seg>,
+    size: IVec2,
+    segments: Vec<Seg>,
+    position: f32,
 }
 
 fn col32(r: u8, g: u8, b: u8) -> u32 {
@@ -25,7 +32,7 @@ fn col32(r: u8, g: u8, b: u8) -> u32 {
 const SEGMENT_LENGTH: i32 = 200; // length of a single segment
 const RUMBLE_LENGTH: i32 = 3; // number of lanes
 const FIELD_OF_VIEW: f32 = 100.0; // angle (degrees) for field of view
-const CAMERA_HEIGHT: f32 = 1000.0; // z height of camera
+const CAMERA_HEIGHT: f32 = 2000.0; // z height of camera
 const ROAD_WIDTH: f32 = 2000.0; // z height of camera
 const DRAW_DISTANCE: i32 = 300;
 
@@ -33,83 +40,134 @@ const DRAW_DISTANCE: i32 = 300;
 impl Roads {
     #[wasm_bindgen(constructor)]
     pub fn new(w: usize, h: usize) -> Self {
+        let mut segments = Vec::with_capacity(500);
+        for n in 0..segments.capacity() {
+            segments.push(Seg {
+                index: n,
+                p1: Vec3::Z * n as f32 * SEGMENT_LENGTH as f32,
+                p2: Vec3::Z * (n + 1) as f32 * SEGMENT_LENGTH as f32,
+            });
+        }
+
+        log(&format!("segs: {}", segments.len()));
+
         Self {
-            w,
-            h,
-            // segments
+            size: ivec2(w as i32, h as i32),
+            segments,
+            position: 0.0,
         }
     }
 
-    fn project(&self, p: Vec3, cam_pos: Vec3, cameraDepth: f32, roadWidth: f32) -> Option<IVec3> {
+    fn project(&self, p: Vec3, cam_pos: Vec3, camera_depth: f32, road_width: f32) -> Option<IVec3> {
         let cam = p - cam_pos;
 
-        let hw = self.w as f32 / 2.0;
-        let hh = self.h as f32 / 2.0;
-        if cam.z <= cameraDepth {
+        let hw = self.size.x as f32 / 2.0;
+        let hh = self.size.y as f32 / 2.0;
+        if cam.z < camera_depth {
             return None;
         }
-        let screen_scale = cameraDepth / cam.z;
+        let screen_scale = camera_depth / cam.z;
         let screen = IVec3::new(
             (hw + screen_scale * cam.x * hw) as i32,
             (hh - screen_scale * cam.y * hh) as i32,
-            (screen_scale * roadWidth * hw) as i32,
+            (screen_scale * road_width * hw) as i32,
         );
         Some(screen)
     }
 
-    pub fn update(&mut self, b: &mut [u32], t: f32) {
-        let camera_depth: f32 = 1.0 / ((FIELD_OF_VIEW / 2.0) * std::f32::consts::PI / 180.0).tan();
-        let resolution: f32 = self.h as f32 / 480.0;
+    fn find_segment(&mut self, z: f32) -> usize {
+        let l = self.segments.len();
+        (z / SEGMENT_LENGTH as f32).floor() as usize % l
+    }
 
-        let playerZ: f32 = CAMERA_HEIGHT * camera_depth;
+    pub fn update(&mut self, b: &mut [u32], time: f32) {
+        let camera_depth: f32 = 1.0 / ((FIELD_OF_VIEW / 2.0) * std::f32::consts::PI / 180.0).tan();
+        let resolution: f32 = self.size.y as f32 / 480.0;
+
+        let player_z: f32 = CAMERA_HEIGHT * camera_depth;
 
         let track_length = 500 * SEGMENT_LENGTH;
+
+        let base_segment = self.find_segment(self.position);
+
         b.fill(0xff00ff00);
-        let mut maxy = self.h as i32;
+        let mut maxy = self.size.y as i32;
+
         for n in 0..DRAW_DISTANCE {
-            let p1 = Vec3::new(0.0, 0.0, (n * SEGMENT_LENGTH) as f32);
-            let p2 = Vec3::new(0.0, 0.0, ((n + 1) * SEGMENT_LENGTH) as f32);
+            let seg = (self.segments[base_segment].index + n as usize) % self.segments.len();
+            log(&format!("n {n} seg {seg} pos {}", self.position));
+
             let dark = (n / RUMBLE_LENGTH) % 2 == 0;
 
             let cam = Vec3::new(
                 0.0 /* playerX */ * ROAD_WIDTH,
                 CAMERA_HEIGHT,
-                0.0, /* position - (segment.looped ? trackLength : 0) */
+                900.0, // t*10.0, /* position - (segment.looped ? trackLength : 0) */
             );
 
-            let s1 = self.project(p1, cam, camera_depth, ROAD_WIDTH);
-            let s2 = self.project(p2, cam, camera_depth, ROAD_WIDTH);
+            let (s1, s2) = {
+                let seg = &self.segments[seg];
+                (
+                    self.project(seg.p1, cam, camera_depth, ROAD_WIDTH),
+                    self.project(seg.p2, cam, camera_depth, ROAD_WIDTH),
+                )
+            };
 
             // behind us
             if let (Some(s1), Some(s2)) = (s1, s2) {
-                if s2.y >= maxy {
+                if s2.y > maxy {
                     // clip by (already rendered) segment
                     continue;
                 }
                 maxy = s2.y;
 
                 let tl = ivec2(
-                    (s2.x - s2.z).clamp(0, self.w as i32 - 1),
-                    s2.y.clamp(0, self.h as i32 - 1),
+                    s2.x - s2.z, //.clamp(0, self.w as i32 - 1),
+                    s2.y,        //.clamp(0, self.h as i32 - 1),
                 );
                 let bl = ivec2(
-                    (s1.x - s1.z).clamp(0, self.w as i32 - 1),
-                    s1.y.clamp(0, self.h as i32 - 1),
+                    s1.x - s1.z, //.clamp(0, self.w as i32 - 1),
+                    s1.y,        //.clamp(0, self.h as i32 - 1),
                 );
                 let tr = ivec2(
-                    (s2.x + s2.z).clamp(0, self.w as i32 - 1),
-                    s2.y.clamp(0, self.h as i32 - 1),
+                    s2.x + s2.z, //.clamp(0, self.w as i32 - 1),
+                    s2.y,        //.clamp(0, self.h as i32 - 1),
                 );
                 let br = ivec2(
-                    (s1.x + s1.z).clamp(0, self.w as i32 - 1),
-                    s1.y.clamp(0, self.h as i32 - 1),
+                    s1.x + s1.z, //.clamp(0, self.w as i32 - 1),
+                    s1.y,        //.clamp(0, self.h as i32 - 1),
                 );
 
+                const RUMBLE_WIDTH: f32 = 0.05;
                 for (l, r) in Bresenham::new((tl.x, tl.y), (bl.x, bl.y))
                     .zip(Bresenham::new((tr.x, tr.y), (br.x, br.y)))
                 {
+                    if l.1 < 0 || l.1 > (self.size.y as i32) - 1 {
+                        break;
+                    }
+                    let lw = r.0 - l.0;
                     for p in Bresenham::new(l, r) {
-                        b[p.1 as usize * self.w + p.0 as usize] = 0xff000000 + (n * 37) as u32 % 255;
+                        if !(0..self.size.x as i32 - 1).contains(&p.0) {
+                            continue;
+                        }
+                        let horizontal_ratio = (p.0 - l.0) as f32 / lw as f32;
+                        b[(p.1 * self.size.x + p.0) as usize] = if horizontal_ratio < RUMBLE_WIDTH
+                            || horizontal_ratio > 1.0 - RUMBLE_WIDTH
+                        {
+                            if dark {
+                                col32(255, 0, 0)
+                            } else {
+                                0xffffffff
+                            }
+                        } else if dark && (0.47..0.53).contains(&horizontal_ratio) {
+                            0xffffffff
+                        } else {
+                            if dark {
+                                0xff555555
+                            } else {
+                                0xffaaaaaa
+                            }
+                        };
                     }
                 }
 
