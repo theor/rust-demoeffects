4d8ae9783ee7dbff740fd02a55200eab4c81a08a
Theo Richart <theor@unity3d.com>
2023-07-27 16:39:04 -0400

    remove floor

--- sample/sample-rust/src/bitmap.rs
+++ sample/sample-rust/src/bitmap.rs
@@ -18,16 +18,16 @@ pub(crate) fn draw_bitmap(
     clip_y: i32,
 ) {
     let (sw, sh) = (
-        (bmp.w as f32 * scale).floor() as i32,
-        (bmp.h as f32 * scale).floor() as i32,
+        (bmp.w as f32 * scale) as i32,
+        (bmp.h as f32 * scale) as i32,
     );
     let inv_scale = 1.0 / scale;
 
     for y in (pos.y).max(0)..(pos.y + sh).min(h as i32).min(clip_y) {
         for x in (pos.x).max(0)..(pos.x + sw).min(w as i32) {
        
-            let px = ((x - pos.x) as f32 * inv_scale).floor() as usize;
-            let py = ((y - pos.y) as f32 * inv_scale).floor() as usize; let c = if h_flip{ bmp.data[bmp.w * py + bmp.w - 1 - px]}else { bmp.data[bmp.w * py + px]};
+            let px = ((x - pos.x) as f32 * inv_scale) as usize;
+            let py = ((y - pos.y) as f32 * inv_scale) as usize; let c = if h_flip{ bmp.data[bmp.w * py + bmp.w - 1 - px]}else { bmp.data[bmp.w * py + px]};
             if c != transparent {
                 b[y as usize*w + x as usize] = c;
             }
